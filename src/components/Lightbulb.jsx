edacab1b2172e8746c63652160aada287758a67d
LauraLyckholm <laura.lyckholm@gmail.com>
2023-09-24 12:22:49 +0200

    Tested excercise using ternary and switch

--- src/components/Lightbulb.jsx
+++ src/components/Lightbulb.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
+
 export const Lightbulb = () => {
   // Initialize the state for on/off
+  const [isLit, setIsLit] = useState(false);
+
+  // Using Swtich Case
+  // const getLightbulbClasses = () => {
+  //   switch (isLit) {
+  //     case true:
+  //       return 'lightbulb on';
+  //     case false:
+  //       return 'lightbulb off';
+  //     default:
+  //       return 'lightbulb';
+  //   }
+  // };
+
+  // Using Ternary operators
+  const getLightbulbClasses = () => {
+    return `lightbulb ${isLit ? 'on' : 'off'}`;
+  };
 
   const toggleLight = () => {
     // Toggle the on/off state
-    console.log("Clicked");
+    setIsLit(!isLit) // When clicked the state changes between true and false 
   };
   return (
     <div className="lightbulb-container">
-      <div className="lightbulb" onClick={toggleLight}>
+      <div className={getLightbulbClasses()} onClick={toggleLight}>
         <div className="bulb"></div>
       </div>
     </div>
